74a12b087fbd4963070cfde703542978751bea97
david <davidzz@bupt.edu.cn>
2021-12-12 09:40:48 +0800

    branch1 commit

---
+++ src/com/david/Main.java
@@ -0,0 +1,11 @@
+package com.david;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello spring!");
+        System.out.println("hello git");
+        System.out.println("this branch1!");
+    }
+}
